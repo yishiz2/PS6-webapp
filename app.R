4d56d512009134b380e12d6346a90459bc7d0f8f
yishiz2 <yishiz2@uw.edu>
2023-03-05 22:58:11 -0800

    update

--- app.R
+++ app.R
@@ -8,43 +8,54 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(htmltools)
+data <- read.delim("UAH-lower-troposphere-long.csv.bz2")
+data
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("UAH"),
+    
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    
+    tabsetPanel(
+      tabPanel("About", 
+               p("This is the content of Tab 1"),
+               p("second line")),
+      tabPanel("Plots", 
+               column(width = 3,
+                      checkboxGroupInput("regions", "Select regions", choices = unique(data$region), selected = "globe"),
+                      checkboxInput("trendline", "Trendline", "Display trend line", value = FALSE)
+                      ),
+               mainPanel(plotOutput("scatterplot"))
+               ),
+      tabPanel("Tables", "This is the content of Tab 3")
     )
-)
+  )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Filter data based on selected regions
+  filtered_data <- reactive({
+    data %>% filter(region %in% input$regions)
+  })
+  
+  # Create scatterplot with filtered data
+  output$scatterplot <- renderPlot({
+    p <- ggplot(filtered_data(), aes(x = year, y = temp, color = region)) +
+      geom_point() +
+      labs(x = "Year", y = "Temperature", color = "Region")
+    
+    if (input$trendline) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
 }
 
 # Run the application 
